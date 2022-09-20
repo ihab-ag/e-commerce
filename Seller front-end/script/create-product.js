abce4ae0b18a895fea31d5e940c013622393c4d6
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-20 23:59:34 +0300

    EC-15 define variable in js

---
+++ Seller front-end/script/create-product.js
@@ -0,0 +1,10 @@
+//define variable
+const navItem1=document.querySelector("#nav-item1");
+const navItem2=document.querySelector("#nav-item2");
+const navItem3=document.querySelector("#nav-item3");
+const navItem4 = document.querySelector("#nav-item4");
+//navbar buttons
+const createProductButton=document.querySelector("#create-p-btn");
+const showProductsButton=document.querySelector("#show-p-btn");
+const statusButton=document.querySelector("#status-btn");
+const CouponsAdsButton = document.querySelector("#coupons-ads-btn");
