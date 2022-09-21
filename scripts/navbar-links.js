5d3afdea78c9a4dd2808bc4d180f5e2cdbbfd6f4
= <motasem.yakhni@gmail.com>
2022-09-21 13:36:01 +0300

    EC-19 make navbar links interactive

--- scripts/navbar-links.js
+++ scripts/navbar-links.js
@@ -2,21 +2,27 @@ const clientPage = document.getElementById('display-clients-table')
 const sellerPage = document.getElementById('display-sellers-table')
 const statPage = document.getElementById('display-stats-graph')
 const sections = document.querySelectorAll('.main-sub-container')
-
+const navbarLinks = document.querySelectorAll('.navbar-link')
 
 clientPage.addEventListener('click', () => {
     sections.forEach(section => section.classList.add('view-none'))
     document.getElementById('client-section').classList.remove('view-none')
+    navbarLinks.forEach(navbarLink => navbarLink.classList.remove('current-navbar-link'))
+    clientPage.classList.add('current-navbar-link')
 })
 
 sellerPage.addEventListener('click', () => {
     sections.forEach(section => section.classList.add('view-none'))
     document.getElementById('seller-section').classList.remove('view-none')
+    navbarLinks.forEach(navbarLink => navbarLink.classList.remove('current-navbar-link'))
+    sellerPage.classList.add('current-navbar-link')
 })
 
 statPage.addEventListener('click', () => {
     sections.forEach(section => section.classList.add('view-none'))
     document.getElementById('stat-section').classList.remove('view-none')
+    navbarLinks.forEach(navbarLink => navbarLink.classList.remove('current-navbar-link'))
+    statPage.classList.add('current-navbar-link')
 })
 
 // clientPage.addEventListener('click', () => {
