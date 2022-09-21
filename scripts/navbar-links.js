5fcb2409d4b7dc9df8b9e5f947c8ba0b57d55cc0
= <motasem.yakhni@gmail.com>
2022-09-21 13:23:20 +0300

    EC-19 add the event listeners in navbar links script

--- scripts/navbar-links.js
+++ scripts/navbar-links.js
@@ -1,3 +1,26 @@
 const clientPage = document.getElementById('display-clients-table')
 const sellerPage = document.getElementById('display-clients-table')
 const statPage = document.getElementById('display-stats-graph')
+const sections = document.querySelectorAll('.main-sub-container')
+
+
+clientPage.addEventListener('click', () => {
+    sections.forEach(section => section.classList.add('view-none'))
+    document.getElementById('client-section').classList.remove('view-none')
+})
+
+sellerPage.addEventListener('click', () => {
+    sections.forEach(section => section.classList.add('view-none'))
+    document.getElementById('seller-section').classList.remove('view-none')
+})
+
+statPage.addEventListener('click', () => {
+    sections.forEach(section => section.classList.add('view-none'))
+    document.getElementById('stat-section').classList.remove('view-none')
+})
+
+// clientPage.addEventListener('click', () => {
+//     document.getElementById('client-section').classList.remove('view-none')
+//     document.getElementById('seller-section').classList.add('view-none')
+//     document.getElementById('stat-section').classList.add('view-none')
+// })
