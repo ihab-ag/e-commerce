877deecdb3f19c8c9b682f9ed69554bf9c8b23b6
= <motasem.yakhni@gmail.com>
2022-09-22 09:39:13 +0300

    EC-20 set error handlers

---
+++ scripts/form-validations.js
@@ -0,0 +1,35 @@
+const nameValidation = (name) => {
+    const exp = /^[A-Za-z\s]{5, 255}$/
+    if(name.length < 5) {
+        return false
+    }else if (name.length > 255) {
+        return false
+    }else if(!exp.match(name)) {
+        return false
+    }
+
+    return true
+}
+
+const emailValidation = (email) => {
+    const exp = /^(\w([\.-]?\w)*)+@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(!exp.match(email)) {
+        return false
+    }
+
+    return true
+}
+
+const phoneValidation = (phone) => {
+    const exp = /^+\d$/
+    if(!exp.match(phone)) {
+        return false
+    }
+
+    return true
+}
+
+
+const setErrorMessage = (message) => {
+
+}
