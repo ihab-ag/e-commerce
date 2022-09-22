650d8324c412897c12364c548b55ada28a08865a
= <motasem.yakhni@gmail.com>
2022-09-23 01:03:13 +0300

    EC-38 update seller adder

--- ecommerce-server/apis/add-seller.php
+++ ecommerce-server/apis/add-seller.php
@@ -5,15 +5,15 @@
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST');
     header('Allow-Control-Allow-Headers: Allow-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
-    $sellerID = $_POST['id'];
     $name = $_POST['name'];
     $pwd = $_POST['pwd'];
     $phone = $_POST['phone'];
     $location = $_POST['location'];
+    $email = $_POST['email'];
     // connect to db
     $query= $mysqli->prepare('INSERT INTO sellers(name, password, phone, location, email) VALUES(?, ?, ?, ?, ?);');
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-    $query->bind_param('sssss', $sellerID, $name, $hashedPwd, $phone, $location);
+    $query->bind_param('sssss', $name, $hashedPwd, $phone, $location, $email);
     // query to put client into the banned list
     if(!$query->execute()) {
         //statement failed
