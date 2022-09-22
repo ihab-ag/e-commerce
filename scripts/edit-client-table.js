8a16df9f37e2b42846a84c7d402337fab4a1b286
= <motasem.yakhni@gmail.com>
2022-09-22 11:18:25 +0300

    EC-69 make client ban btn interactive

---
+++ scripts/edit-client-table.js
@@ -0,0 +1,38 @@
+const banClientBtn = document.getElementById('ban-client-1')
+const unBanClientBtn = document.getElementById('unban-client-1')
+const banClientConfirmBtn = document.getElementById('ban-client-1-confirm')
+
+banClientBtn.addEventListener('click', () => {
+    banClientConfirmBtn.classList.remove('view-none')
+    banClientBtn.classList.add('view-none')
+    unBanClientBtn.classList.add('view-none')
+})
+
+banClientConfirmBtn.addEventListener('mouseleave', () => {
+    console.log('mouse-leave')
+    if(unBanClientBtn.classList.contains('view-none')) {
+        console.log("if")
+        banClientBtn.classList.remove('view-none')
+        banClientConfirmBtn.classList.add('view-none')
+    }else {
+        console.log('else')
+        banClientBtn.classList.add('view-none')
+    }
+})
+
+banClientConfirmBtn.addEventListener('click', () => {
+    unBanClientBtn.classList.remove('view-none')
+    if(!banClientBtn.classList.contains('view-none')) {
+        console.log("No view-none")
+        banClientBtn.classList.add('view-none')
+    }
+    console.log("Yes view-none")
+    banClientConfirmBtn.classList.add('view-none')
+})
+
+
+unBanClientBtn.addEventListener('click', () => {
+    banClientBtn.classList.remove('view-none')
+    unBanClientBtn.classList.add('view-none')
+})
+
