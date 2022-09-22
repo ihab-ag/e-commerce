878a5fc2d330cac2c1b65068ef4da3eef75f386e
= <motasem.yakhni@gmail.com>
2022-09-22 10:40:10 +0300

    EC-69 update delete btn interactiveness

--- scripts/edit-table.js
+++ scripts/edit-table.js
@@ -1,13 +1,14 @@
 const editBtn = document.getElementById('btn-edit-1')
 const deleteBtn = document.getElementById('btn-delete-1')
 const editConfirmBtn = document.getElementById('btn-edit-confirm')
-const banConfirmBtn = document.getElementById('btn-delete-confirm')
+const deleteConfirmBtn = document.getElementById('btn-delete-confirm')
 const info = document.querySelectorAll('.btn-seller-1')
 
 editBtn.addEventListener('click', () => {
     info.forEach(box => box.removeAttribute('disabled'))
     editConfirmBtn.classList.remove('view-none')
     editBtn.classList.add('view-none')
+    
 })
 
 editConfirmBtn.addEventListener('click', () => {
@@ -17,18 +18,25 @@ editConfirmBtn.addEventListener('click', () => {
 })
 
 deleteBtn.addEventListener('click', () => {
-    banConfirmBtn.classList.remove('view-none')
+    deleteConfirmBtn.classList.remove('view-none')
     deleteBtn.classList.add('view-none')
+    info.forEach(box => box.setAttribute('disabled', true))
+    editConfirmBtn.classList.add('view-none')
+    editBtn.classList.remove('view-none')
 })
 
-banConfirmBtn.addEventListener('click', () => {
+deleteConfirmBtn.addEventListener('click', () => {
     document.getElementById('seller-row-1').classList.add('view-none')
+    editConfirmBtn.classList.add('view-none')
+    editBtn.classList.remove('view-none')
+    editConfirmBtn.classList.add('view-none')
+    editBtn.classList.remove('view-none')
 })
 
-banConfirmBtn.addEventListener('mouseleave', () => {
+deleteConfirmBtn.addEventListener('mouseleave', () => {
     info.forEach(box => box.setAttribute('disabled', true))
     editConfirmBtn.classList.add('view-none')
     editBtn.classList.remove('view-none')
     deleteBtn.classList.remove('view-none')
-    banConfirmBtn.classList.add('view-none')
+    deleteConfirmBtn.classList.add('view-none')
 })
