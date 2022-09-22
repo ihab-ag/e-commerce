3746eb6a2176faa03ac97d24b48b1e52f0acc1bb
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-22 13:54:13 +0300

    fixed query for date format

--- ecommerce-server/apis/view-clients.php
+++ ecommerce-server/apis/view-clients.php
@@ -1,7 +1,15 @@
 <?php
     include('connection.php');
     // connect to db
-    $query= $mysqli->prepare("SELECT `id`, `name`, `email`, `phone`, `joined_date` FROM `clients` LIMIT 10");
+    $query= $mysqli->prepare("SELECT
+    `id`,
+    `name`,
+    `email`,
+    `phone`,
+    DATE_FORMAT(`joined_date`,"%b, %e, %Y") AS joined_date
+FROM
+    `clients`
+LIMIT 10;");
     // query to get clients
     $query->execute();
     $array = $query->get_result();
