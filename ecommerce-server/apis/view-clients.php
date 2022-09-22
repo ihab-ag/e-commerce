7953537428cc5feb608a31c58e3619ded0f58521
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-22 13:37:04 +0300

    EC-37 wrote query

--- ecommerce-server/apis/view-clients.php
+++ ecommerce-server/apis/view-clients.php
@@ -1,4 +1,6 @@
 <?php
     include('connection.php');
     // connect to db
+    $query= $mysqli->prepare("SELECT `id`, `name`, `email`, `phone`, `joined_date` FROM `clients` LIMIT 10");
+    // query to get clients
 ?>
