9f37c93cb7c7154fdf0f200be1a4560af1bedc57
= <motasem.yakhni@gmail.com>
2022-09-23 01:32:11 +0300

    creat add seller script

---
+++ scripts/add-seller.js
@@ -0,0 +1,11 @@
+const addSellerUrl = "http://localhost/9-sefactory/e-commerce/ecommerce-server/apis/add-seller.php"
+const sellerName = document.getElementById('add-seller-name').value
+const sellerEmail = document.getElementById('add-seller-email').value
+const sellerPhone = document.getElementById('add-seller-phone').value
+const sellerLocation = document.getElementById('add-seller-location').value
+const sellerPassword = document.getElementById('add-seller-pwd').value
+
+const addBtn = document.getElementById('add-btn')
+const addSellerCaller = document.getElementById('add-table-caller')
+const addSellerCanceler = document.getElementById('add-seller-canceler')
+
