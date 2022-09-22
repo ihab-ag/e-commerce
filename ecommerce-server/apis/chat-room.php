4f3ff662d93e21dca98c440a49844a55bd93ce77
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-22 22:55:59 +0300

    EC-71 check if room exists

--- ecommerce-server/apis/chat-room.php
+++ ecommerce-server/apis/chat-room.php
@@ -4,5 +4,15 @@
     header('Content-Type: application/json');
     include('connection.php');
     // connect to db
+    $client_id = $_POST['client_id'];
+    $seller_id = $_POST['seller_id'];
+    // get post params
+    $query= $mysqli->prepare("SELECT id FROM chat_rooms WHERE sellers_id= ? AND clients_id= ?");
+    $query->bind_param("ii",$seller_id,$client_id);
+    $query->execute();
+    $array=$query->get_result();
+    // check if chat room exists
+    
+    
     
 ?>
