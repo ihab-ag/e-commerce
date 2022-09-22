d5322811fe5d3668a0914e55cc4947ad0050fead
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-22 13:40:12 +0300

    EC-37 get results

--- ecommerce-server/apis/view-clients.php
+++ ecommerce-server/apis/view-clients.php
@@ -3,4 +3,7 @@
     // connect to db
     $query= $mysqli->prepare("SELECT `id`, `name`, `email`, `phone`, `joined_date` FROM `clients` LIMIT 10");
     // query to get clients
+    $query->excute();
+    $array = $query->get_result();
+    // get results
 ?>
