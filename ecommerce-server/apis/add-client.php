4a0d5f0821fe5cc634da896273167e7277750da1
= <motasem.yakhni@gmail.com>
2022-09-23 22:42:14 +0300

    EC-34 create add client api

--- ecommerce-server/apis/add-client.php
+++ ecommerce-server/apis/add-client.php
@@ -8,17 +8,17 @@
     $name = $_POST['name'];
     $pwd = $_POST['pwd'];
     $phone = $_POST['phone'];
-    $location = $_POST['location'];
+    //$location = $_POST['location'];
     $email = $_POST['email'];
     // connect to db
-    $query= $mysqli->prepare('INSERT INTO sellers(name, password, phone, location, email) VALUES(?, ?, ?, ?, ?);');
+    $query= $mysqli->prepare('INSERT INTO clients(name, password, phone, email) VALUES(?, ?, ?, ?);');
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-    $query->bind_param('sssss', $name, $hashedPwd, $phone, $location, $email);
-    // query to put seller into the banned list
+    $query->bind_param('ssss', $name, $hashedPwd, $phone, $email);
+    // query to put client into the banned list
     if(!$query->execute()) {
         //statement failed
         // echo json_encode(["success" => false]);
-        die("Error in add-seller-api");
+        die("Error in add-client-api");
     }
     //statement succeeded
     // echo json_encode(["success" => true]);
@@ -26,12 +26,12 @@
     $query2 = $mysqli->prepare('SELECT id FROM sellers WHERE email=?;');
     $query2->bind_param('s', $email);
     if(!$query2->execute()) {
-        die("Error in add-seller-api selection part");
+        die("Error in add-client-api selection part");
     }
     $results = $query2->get_result();
     $result = $results->fetch_assoc();
-    $sellerID = $result['id']; //guaranteed one answer
+    $clientID = $result['id']; //guaranteed one answer
 
-    echo json_encode(["success" => true, "seller_id" => $sellerID]);
+    echo json_encode(["success" => true, "client_id" => $clientID]);
 
 ?>
