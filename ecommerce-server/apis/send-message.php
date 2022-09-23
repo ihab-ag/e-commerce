dac410d687c39a654d8e920c5aa5ef63a12bceff
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-23 08:17:25 +0300

    EC-72 send mesages api

--- ecommerce-server/apis/send-message.php
+++ ecommerce-server/apis/send-message.php
@@ -6,7 +6,12 @@
     $room_id=$_POST['room_id'];
     $sender=$_POST['sender'];
     $message=$_POST['message'];
-    $date = new DateTime();
-    $date->format('Y-m-d H:i:s');
+    $d = new DateTime();
+    $date=$d->format('Y-m-d H:i:s');
     // get values
+    $query=$mysqli->prepare("INSERT INTO `messages` (`sender`, `message`, `date`, `chats_rooms_id`) 
+    VALUES (? , ? , ? , ?);");
+    $query->bind_param("sssi",$sender,$message,$date,$room_id);
+    $query->execute();
+    // execute query
 ?>
