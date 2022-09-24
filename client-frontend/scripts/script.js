86886b8cc3a494ca27656ffd086da4e41f28f7fa
daliakhalifa02 <daliakhalifa02@gmail.com>
2022-09-24 18:38:51 +0300

    Added hamburger menu to mobile

--- client-frontend/scripts/script.js
+++ client-frontend/scripts/script.js
@@ -31,3 +31,32 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+//Hamburger Menu
+
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+
+items.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
