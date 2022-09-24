f07212870a48ea4187a6e5190678b905b0301c00
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-24 08:54:10 +0300

    check if user exists

--- ecommerce-server/apis/login-auth.php
+++ ecommerce-server/apis/login-auth.php
@@ -5,6 +5,17 @@
     include("connection.php");
     // connect db
     $email=$_POST['email'];
-    $pass=$_POST['pass'];
+    $password=$_POST['password'];
     // get info
+    $query= $mysqli->prepare("SELECT `id` FROM `clients` WHERE email=? AND password=?");
+    $query->bind_param("ss",$email,$password);
+    $query->execute();
+    $array=$query->get_result();
+    // check if use exists
+    if($a=$array->fetch_assoc()){
+        $id=$a['id'];
+        
+    }
+    else
+    echo "nope";
 ?>
