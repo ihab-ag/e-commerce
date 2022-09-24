8eeb671927182de70863a9a9acde50084f325057
= <motasem.yakhni@gmail.com>
2022-09-24 11:52:44 +0300

    link db data with seller graphs

--- scripts/draw-graphs.js
+++ scripts/draw-graphs.js
@@ -50,98 +50,222 @@ window.addEventListener('resize', () => {
     
 })
 
-const bestSellerWeekGraph = new Chart('best-seller-week', {
-    type: 'bar',
-    data: bestSellerData,
-    options: {
-        scales: {
-            yAxes: [{ticks: {min: 0, stepSize: 10000}}]
-        },
-        title: {
-            display: true,
-            text: "Best Seller of the Week",
-        },
-        legend: {
-            position: 'top',
-            labels: {
-                fontColor: '#000'
-            },
-        },
-        // layout: {
-        //     padding: {
-        //         left: 10,
-        //         right: 0,
-        //         bottom: 0,
-        //     }
-        // },
-        tooltips: {
-            //when hovered
-            enabled: true
-        }
-    }
-})
+const bestWeekUrl = "http://localhost/9-sefactory/e-commerce/ecommerce-server/apis/best-seller-week.php"
 
-const bestSellerMonthGraph = new Chart('best-seller-month', {
-    type: 'bar',
-    data: bestSellerData,
-    options: {
-        scales: {
-            yAxes: [{ticks: {min: 0, stepSize: 10000}}]
-        },
-        title: {
-            display: true,
-            text: "Best Seller of the Month",
-        },
-        legend: {
-            position: 'top',
-            labels: {
-                fontColor: '#000'
-            },
-        },
-        // layout: {
-        //     padding: {
-        //         left: 10,
-        //         right: 0,
-        //         bottom: 0,
-        //     }
-        // },
-        tooltips: {
-            //when hovered
-            enabled: true
-        }
-    }
-})
+const getBestSellerWeek = () => {
+    axios.get(bestWeekUrl).then(response => {
+        const weeklyData = response.data
+        const labelsArr = []
+        const dataArr = []
+        //console.log(weeklyData)
+        weeklyData.forEach(data => {
+            labelsArr.push(data['name'])
+            dataArr.push(data['tot_amount'])
+            
+        })
 
-const bestSellerYearGraph =  new Chart('best-seller-year', {
-    type: 'bar',
-    data: bestSellerData,
-    options: {
-        scales: {
-            yAxes: [{ticks: {min: 0, stepSize: 10000}}]
-        },
-        title: {
-            display: true,
-            text: "Best Seller of the Year",
-        },
-        legend: {
-            position: 'top',
-            labels: {
-                fontColor: '#000'
-            },
-        },
-        // layout: {
-        //     padding: {
-        //         left: 10,
-        //         right: 0,
-        //         bottom: 0,
-        //     }
-        // },
-        tooltips: {
-            //when hovered
-            enabled: true
+        const bestWeekSellerData = {
+            labels: labelsArr,
+            datasets: [{
+                label: 'Amount (in $)',
+                data: dataArr,
+                backgroundColor: [
+                    'rgba(27, 70, 89, 0.2)',
+                    'rgba(50, 201, 156, 0.2)',
+                    'rgba(255, 215, 0, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(27, 70, 89, 1)',
+                    'rgba(50, 201, 156, 1)',
+                    'rgba(255, 215, 0, 1)',
+                ],
+                borderWidth: 1,
+                hoverBorderWidth: 3,
+                hoverBackgroundColor: [
+                    'rgba(27, 70, 89, 0.6)',
+                    'rgba(50, 201, 156, 0.6)',
+                    'rgba(255, 215, 0, 0.6)',
+                ]
+            }],
         }
-    }
-})
+        const bestSellerWeekGraph = new Chart('best-seller-week', {
+            type: 'bar',
+            data: bestWeekSellerData,
+            options: {
+                scales: {
+                    yAxes: [{ticks: {min: 0, max: (parseInt(dataArr[0]))}}]
+                },
+                title: {
+                    display: true,
+                    text: "Best Seller of the Week",
+                },
+                legend: {
+                    position: 'top',
+                    labels: {
+                        fontColor: '#000'
+                    },
+                },
+                // layout: {
+                //     padding: {
+                //         left: 10,
+                //         right: 0,
+                //         bottom: 0,
+                //     }
+                // },
+                tooltips: {
+                    //when hovered
+                    enabled: true
+                }
+            }
+        })
+    })
+}
+//console.log(bestSellerData['datasets'][0]['data'])
+getBestSellerWeek()
+
+const bestMonthUrl = "http://localhost/9-sefactory/e-commerce/ecommerce-server/apis/best-seller-month.php"
+
+const getBestSellerMonth = () => {
+    axios.get(bestMonthUrl).then(response => {
+        const monthlyData = response.data
+        const labelsArr = []
+        const dataArr = []
+        //console.log(monthlyData)
+        monthlyData.forEach(data => {
+            labelsArr.push(data['name'])
+            dataArr.push(data['tot_amount'])
+            
+        })
+        const bestMonthSellerData = {
+            labels: labelsArr,
+            datasets: [{
+                label: 'Amount (in $)',
+                data: dataArr,
+                backgroundColor: [
+                    'rgba(27, 70, 89, 0.2)',
+                    'rgba(50, 201, 156, 0.2)',
+                    'rgba(255, 215, 0, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(27, 70, 89, 1)',
+                    'rgba(50, 201, 156, 1)',
+                    'rgba(255, 215, 0, 1)',
+                ],
+                borderWidth: 1,
+                hoverBorderWidth: 3,
+                hoverBackgroundColor: [
+                    'rgba(27, 70, 89, 0.6)',
+                    'rgba(50, 201, 156, 0.6)',
+                    'rgba(255, 215, 0, 0.6)',
+                ]
+            }],
+        }
+        const bestSellerMonthGraph = new Chart('best-seller-month', {
+            type: 'bar',
+            data: bestMonthSellerData,
+            options: {
+                scales: {
+                    yAxes: [{ticks: {min: 0, max: parseInt(dataArr[0])}}]
+                },
+                title: {
+                    display: true,
+                    text: "Best Seller of the Month",
+                },
+                legend: {
+                    position: 'top',
+                    labels: {
+                        fontColor: '#000'
+                    },
+                },
+                // layout: {
+                //     padding: {
+                //         left: 10,
+                //         right: 0,
+                //         bottom: 0,
+                //     }
+                // },
+                tooltips: {
+                    //when hovered
+                    enabled: true
+                }
+            }
+        })
+    })
+}
+
+getBestSellerMonth()
+
+const bestYearUrl = "http://localhost/9-sefactory/e-commerce/ecommerce-server/apis/best-seller-year.php"
+
+const getBestSellerYear = () => {
+    axios.get(bestYearUrl).then(response => {
+        const monthlyData = response.data
+        const labelsArr = []
+        const dataArr = []
+        //console.log(monthlyData)
+        monthlyData.forEach(data => {
+            labelsArr.push(data['name'])
+            dataArr.push(data['tot_amount'])
+            
+        })
+        const bestSellerYearData = {
+            labels: labelsArr,
+            datasets: [{
+                label: 'Amount (in $)',
+                data: dataArr,
+                backgroundColor: [
+                    'rgba(27, 70, 89, 0.2)',
+                    'rgba(50, 201, 156, 0.2)',
+                    'rgba(255, 215, 0, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(27, 70, 89, 1)',
+                    'rgba(50, 201, 156, 1)',
+                    'rgba(255, 215, 0, 1)',
+                ],
+                borderWidth: 1,
+                hoverBorderWidth: 3,
+                hoverBackgroundColor: [
+                    'rgba(27, 70, 89, 0.6)',
+                    'rgba(50, 201, 156, 0.6)',
+                    'rgba(255, 215, 0, 0.6)',
+                ]
+            }],
+        }
+        const bestSellerYearGraph = new Chart('best-seller-year', {
+            type: 'bar',
+            data: bestSellerYearData,
+            options: {
+                scales: {
+                    yAxes: [{ticks: {min: 0, max: parseInt(dataArr[0])}}]
+                },
+                title: {
+                    display: true,
+                    text: "Best Seller of the Year",
+                },
+                legend: {
+                    position: 'top',
+                    labels: {
+                        fontColor: '#000'
+                    },
+                },
+                // layout: {
+                //     padding: {
+                //         left: 10,
+                //         right: 0,
+                //         bottom: 0,
+                //     }
+                // },
+                tooltips: {
+                    //when hovered
+                    enabled: true
+                }
+            }
+        })
+    })
+}
+
+getBestSellerYear()
 
 const bestClientWeekGraph =  new Chart('best-client-week', {
     type: 'bar',
