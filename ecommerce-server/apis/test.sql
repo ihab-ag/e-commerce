aeb4b0f93258cdb5b6992c9a18d8606c3624a610
= <motasem.yakhni@gmail.com>
2022-09-24 13:57:43 +0300

    prepare total sellers api

--- ecommerce-server/apis/test.sql
+++ ecommerce-server/apis/test.sql
@@ -7,3 +7,38 @@ LIMIT 3;
 
 select * from revenues
 where date between now() - interval 7 day and now();
+
+select r.date, count(s.id)
+FROM revenues r, sellers s
+WHERE s.id=r.sellers_id
+GROUP BY r.date
+
+select r.date, count(s.id)
+FROM revenues r, sellers s
+WHERE s.id=r.sellers_id and r.date between (now() - interval 366 day) and (now() - interval 276 day)
+GROUP BY r.date 
+ORDER BY r.date DESC
+
+select r.date, count(s.id)
+FROM revenues r, sellers s
+WHERE s.id=r.sellers_id and r.date between (now() - interval 276 day) and (now() - interval 186 day)
+GROUP BY r.date 
+ORDER BY r.date DESC
+
+select r.date, count(s.id)
+FROM revenues r, sellers s
+WHERE s.id=r.sellers_id and r.date between (now() - interval 186 day) and (now() - interval 96 day)
+GROUP BY r.date 
+ORDER BY r.date DESC
+
+select r.date, count(s.id)
+FROM revenues r, sellers s
+WHERE s.id=r.sellers_id and r.date between (now() - interval 96 day) and now()
+GROUP BY r.date
+ORDER BY r.date DESC
+
+-- select r.date, count(s.id)
+-- FROM revenues r, sellers s
+-- WHERE s.id=r.sellers_id and r.date between now() and (now() + interval 96 day)
+-- GROUP BY r.date
+-- ORDER BY r.date DESC
