a49bd8cc72156be5c9f5633982435407dc884e23
= <motasem.yakhni@gmail.com>
2022-09-24 10:57:12 +0300

    EC-34 rename best seller of day to best seller of the week

--- ecommerce-server/apis/best-seller-week.php
+++ ecommerce-server/apis/best-seller-week.php
@@ -7,7 +7,7 @@
     // connect to db
     $query= $mysqli->prepare('SELECT r.sellers_id, s.name, sum(r.amount) as tot_amount
     FROM revenues r, sellers s
-    WHERE date_format(r.date, "%e")=date_format(sysdate(), "%e") AND s.id=r.sellers_id
+    WHERE r.date between now() - interval 7 day and now() AND s.id=r.sellers_id
     GROUP BY sellers_id
     ORDER BY (tot_amount) DESC
     LIMIT 3;
--- ecommerce-server/apis/test.sql
+++ ecommerce-server/apis/test.sql
@@ -4,3 +4,6 @@ WHERE date_format(r.date, "%Y")=date_format(sysdate(), "%Y") AND s.id=r.sellers_
 GROUP BY sellers_id
 ORDER BY (tot_amount) DESC
 LIMIT 3;
+
+select * from revenues
+where date between now() - interval 7 day and now();
