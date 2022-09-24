ddaf1bbe647594c7ca81b0af39e796a700285075
= <motasem.yakhni@gmail.com>
2022-09-24 15:00:43 +0300

    EC-34 create latest signed sellers api

--- ecommerce-server/apis/test.sql
+++ ecommerce-server/apis/test.sql
@@ -12,30 +12,31 @@ select r.date, count(s.id)
 FROM revenues r, sellers s
 WHERE s.id=r.sellers_id
 GROUP BY r.date
+------------------------
+select joined_date, count(id) as sellers_count
+FROM sellers
+WHERE joined_date between (now() - interval 366 day) and (now() - interval 276 day)
+GROUP BY joined_date 
+ORDER BY joined_date DESC
 
-select r.date, count(s.id)
-FROM revenues r, sellers s
-WHERE s.id=r.sellers_id and r.date between (now() - interval 366 day) and (now() - interval 276 day)
-GROUP BY r.date 
-ORDER BY r.date DESC
+select joined_date, count(id) as sellers_count
+FROM sellers
+WHERE joined_date between (now() - interval 276 day) and (now() - interval 186 day)
+GROUP BY joined_date 
+ORDER BY joined_date DESC
 
-select r.date, count(s.id)
-FROM revenues r, sellers s
-WHERE s.id=r.sellers_id and r.date between (now() - interval 276 day) and (now() - interval 186 day)
-GROUP BY r.date 
-ORDER BY r.date DESC
+select joined_date, count(id) as sellers_count
+FROM sellers
+WHERE joined_date between (now() - interval 186 day) and (now() - interval 96 day)
+GROUP BY joined_date 
+ORDER BY joined_date DESC
 
-select r.date, count(s.id)
-FROM revenues r, sellers s
-WHERE s.id=r.sellers_id and r.date between (now() - interval 186 day) and (now() - interval 96 day)
-GROUP BY r.date 
-ORDER BY r.date DESC
-
-select r.date, count(s.id)
-FROM revenues r, sellers s
-WHERE s.id=r.sellers_id and r.date between (now() - interval 96 day) and now()
-GROUP BY r.date
-ORDER BY r.date DESC
+select joined_date, count(id) as sellers_count
+FROM sellers
+WHERE joined_date between (now() - interval 96 day) and now()
+GROUP BY joined_date 
+ORDER BY joined_date DESC
+------------------------
 
 -- select r.date, count(s.id)
 -- FROM revenues r, sellers s
