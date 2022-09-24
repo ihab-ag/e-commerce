00568fcff4e972cbecf3607de5ec96106ed6bde9
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-24 15:29:22 +0300

    get fav category

--- ecommerce-server/apis/get-sections.php
+++ ecommerce-server/apis/get-sections.php
@@ -4,11 +4,16 @@
     include('connection.php');
     // connect to db
     include('validate-jwt.php');
-
+    // validate token
     if($validity=='valid'){
-        
+        // get fav category
+        $query=$mysqli->prepare("SELECT DISTINCT categories.name FROM favourits, products, categories WHERE favourits.client_id= ? AND favourits.products_id=products.id AND products.Categories_id=categories.id LIMIT 1;");
+        $query->bind_param("i",$id);
+        $query->execute();
+        $array=$query->get_result();
+        $category_name=$array->fetch_assoc()['name'];
+        echo $category_name;
     }
     else
     echo 'invalid token';
-
 ?>
