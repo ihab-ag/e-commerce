9b0868e7c14a7f637c391a126cb050a22854ed89
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-24 16:06:31 +0300

    get products

--- ecommerce-server/apis/get-random-section.php
+++ ecommerce-server/apis/get-random-section.php
@@ -3,5 +3,13 @@
      header('Content-Type: application/json');
      include('connection.php');
      // connect to db
-     
+     $query= $mysqli->prepare('SELECT products.id, products.name, products.description, products.price, products.image
+     FROM products LIMIT 5');
+     $query->execute();
+     $array=$query->get_result();
+     $response=[];
+     while($a=$array->fetch_assoc()){
+        $response[]=$a;
+     }
+    //  get products
 ?>
