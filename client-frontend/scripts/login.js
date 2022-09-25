6061fd3ee41ba59a1d8b30e7a24ad92c2ec686b5
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-25 13:41:39 +0300

    login authentication

--- client-frontend/scripts/login.js
+++ client-frontend/scripts/login.js
@@ -1,9 +1,9 @@
 window.onload=()=>{
     // declarables
     // elements
-    const email=document.getElementById('login-email');
+    const email=document.getElementById('login-username');
     const password=document.getElementById('login-password');
-
+    const signInBtn=document.getElementById('sign-in-btn');
     // functions
     const login=()=>{
         const formData = new FormData();
@@ -11,8 +11,20 @@ window.onload=()=>{
         formData.append('password',password.value);
         // place inputs in form
          axios.post('../ecommerce-server/apis/login-auth.php',formData).then((response)=>{
-            console.log(response);
-         })
+            if(response.data=='Wrong user credentials'){
+                // change div here
+            }
+            else{
+                localStorage.setItem('id', response.data);
+                location.replace("index.html")
+            }
+         }).catch(function (error) {
+            console.log(error);
+          });
+    }
+    // main
+    signInBtn.onclick=()=>{
+        login();
     }
 
 }
