f0a6adb55fed8c6675a5866ad78aa52bd8db159e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-25 12:55:23 +0300

    got inputs

---
+++ client-frontend/scripts/login.js
@@ -0,0 +1,16 @@
+windows.onload=()=>{
+    // declarables
+    // elements
+    const email=document.getElementById('login-email');
+    const password=document.getElementById('login-password');
+
+    // functions
+    const login=()=>{
+        const formData = new FormData();
+        formData.append('email',email.value);
+        formData.append('password',password.value);
+        // place inputs in form
+        
+    }
+
+}
