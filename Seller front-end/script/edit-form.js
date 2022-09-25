a9722b30ad36a8666f9f66fba40f491bd7aaebc8
= <motasem.yakhni@gmail.com>
2022-09-25 11:47:02 +0300

    prepare getting all seller products

--- Seller front-end/script/edit-form.js
+++ Seller front-end/script/edit-form.js
@@ -1,6 +1,7 @@
 const closeEditForm = document.getElementById('close-edit-form')
 const editForm = document.querySelector('.popup-form')
 
-closeEditForm.addEventListener('click', () => {
+closeEditForm.addEventListener('click', (e) => {
+    e.preventDefault()
     editForm.classList.add('view-none')
 })
---
+++ Seller front-end/script/get-seller-products.js
@@ -0,0 +1,1 @@
+const getSellerProductUrl = "http://localhost/9-sefactory/e-commerce/ecommerce-server/apis/get-products.php"
