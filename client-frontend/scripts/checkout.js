1e5780e151533671765d31535257449234555c2c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-26 15:27:12 +0300

    get items from cart

--- client-frontend/scripts/checkout.js
+++ client-frontend/scripts/checkout.js
@@ -11,3 +11,26 @@ window.onclick = function(e) {
     }
   }
 }
+window.onload=()=>{
+  // declare elements
+  const bigContainer=document.getElementById('checkout-container');
+  for(const item of localStorage.getItem('cart').split(",")){
+    const form=new FormData;
+        form.append('product_id',item);
+        axios.post('http://localhost/e-commerce/ecommerce-server/apis/get-product.php', 
+        form
+        ).then(async function (response) {
+           for(const product of response.data){
+            console.log(product['price']);
+            await createContainer(product['image'],product['name'],product['price']);
+          }
+        })
+  }
+  // create product container
+  const createContainer=(img,name,price)=>{
+    const productContainer= document.createElement("div");
+    productContainer.classList="products-container";
+    productContainer.innerHTML='<div class="product"><div class="left-part"><div class="image"><img src="'+img+'" alt="image"></div> <div class="info"><h3>'+name+'</h3></div></div><div class="right-part-container"><div class="right-part"><p class="prices">'+price+'</p><img class="times" src="images/times-icon.png" alt="image"><input type="number" class="counter"></div><div class="trash-icon"><img src="images/trash-icon.png" alt="image"></div></div></div>';
+    bigContainer.appendChild(productContainer);
+  }
+}
